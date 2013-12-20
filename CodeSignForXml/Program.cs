3b7388e3b39bce2ad47ece4382cde669aa78704c
jhenriks79 <justin.henriksen@gmail.com>
2013-12-20 13:24:39 -0800

    Adding project to source control.

---
+++ CodeSignForXml/Program.cs
@@ -0,0 +1,262 @@
+﻿// -----------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// This code is provided AS IS without warranty of any kind. 
+// -----------------------------------------------------------------------
+
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Cryptography.Xml;
+using System.Text;
+using System.Xml;
+
+namespace CodeSignForXml
+{
+    internal class Program
+    {
+        private const string XmlDsigMoreRsaHa256Url = "http://www.w3.org/2001/04/xmldsig-more#rsa-sha256";
+        private const string XmlEncSha256Url = "http://www.w3.org/2001/04/xmlenc#sha256";
+
+        private static void Main(string[] args)
+        {
+            string signingMethod;
+            string inputFile;
+            string xmlString;
+            string signedXml;
+            X509Certificate2 privateCert = null;
+            X509Certificate2 publicCert = null;
+            bool pfxKey;
+
+            if (args.Length < 4)
+            {
+                Console.WriteLine(
+                    "Pfx Usage: SignXml.exe PFX {signMethod} {cert.pfx} {publicCert.cer} {signable xml} {signMethod}: SHA1 | SHA256 | SHA384 | SHA512");
+                Console.WriteLine(
+                    "Token Usage: SignXml.exe TOKEN {signMethod} {publicCert.cer} {signable xml} {signMethod}: SHA1 | SHA256 | SHA384 | SHA512");
+                return;
+            }
+
+            if (args[0] == "PFX")
+            {
+                Console.Write("Input password:");
+                string pswd = Console.ReadLine().Trim();
+
+                signingMethod = args[1];
+
+                privateCert = new X509Certificate2(args[2], pswd, X509KeyStorageFlags.Exportable);
+                publicCert = new X509Certificate2(args[3]);
+
+                inputFile = Path.GetFullPath(args[4]);
+                xmlString = File.ReadAllText(inputFile);
+
+                pfxKey = true;
+            }
+            else
+            {
+                signingMethod = args[1];
+
+                publicCert = new X509Certificate2(args[2]);
+
+                inputFile = Path.GetFullPath(args[3]);
+                xmlString = File.ReadAllText(inputFile);
+
+                var myStore = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+                myStore.Open(OpenFlags.ReadOnly);
+
+                foreach (X509Certificate2 cert in myStore.Certificates)
+                {
+                    if (cert.GetCertHashString() == publicCert.GetCertHashString())
+                    {
+                        privateCert = cert;
+                        break;
+                    }
+                }
+
+                pfxKey = false;
+            }
+
+            XmlDsigUtils.RegisterCryptoAlgorithms();
+
+            if (StringComparer.InvariantCultureIgnoreCase.Compare(signingMethod, "SHA1") != 0)
+            {
+                XmlDsigSigningConfig config = XmlDsigUtils.GetSigningConfig(signingMethod);
+                if (config == null)
+                {
+                    Console.WriteLine(
+                        "SignMethod {0} is not supported. supported methods are SHA1 | SHA256 | SHA384 | SHA512",
+                        signingMethod);
+                    return;
+                }
+
+                signedXml = SignSHA256(pfxKey, xmlString, privateCert, publicCert, config);
+            }
+            else
+            {
+                signedXml = Sign(xmlString, privateCert, publicCert);
+            }
+
+            string outputFile = Path.Combine(Path.GetDirectoryName(inputFile),
+                signingMethod + "Signed" + Path.GetFileName(inputFile));
+
+            File.WriteAllText(outputFile, signedXml);
+
+            Console.WriteLine("Signed xml being written to {0}", outputFile);
+        }
+
+
+        /// <summary>
+        ///     Sign an XML blob and retrun the signed xml blob with signature.
+        /// </summary>
+        /// <param name="xmlString"></param>
+        /// <param name="privateCert"></param>
+        /// <param name="publicCert"></param>
+        /// <returns></returns>
+        public static string SignSHA256(bool pfxKey, string xmlString, X509Certificate2 privateCert,
+            X509Certificate publicCert, XmlDsigSigningConfig config)
+        {
+            // Load an XML file into the XmlDocument object.
+            var xmlDoc = new XmlDocument();
+            xmlDoc.PreserveWhitespace = true;
+            using (var reader = new StringReader(xmlString))
+            {
+                using (var xmlReader = new XmlTextReader(reader))
+                {
+                    xmlReader.DtdProcessing = DtdProcessing.Prohibit;
+                    xmlDoc.Load(xmlReader);
+                }
+            }
+
+            RSACryptoServiceProvider key;
+            if (pfxKey)
+            {
+                var cspParams = new CspParameters(24);
+                cspParams.KeyContainerName = "XML_DISG_RSA_KEY";
+                key = new RSACryptoServiceProvider(cspParams);
+                key.FromXmlString(privateCert.PrivateKey.ToXmlString(true));
+            }
+            else
+            {
+                key = (RSACryptoServiceProvider) privateCert.PrivateKey;
+            }
+
+            // Create a SignedXml object.
+            var signedXml = new SignedXml(xmlDoc);
+
+            // Add the key to the SignedXml document.
+            signedXml.SigningKey = key;
+            signedXml.SignedInfo.CanonicalizationMethod = SignedXml.XmlDsigExcC14NTransformUrl;
+            signedXml.SignedInfo.SignatureMethod = config.SignatureMethod;
+
+            // Create a reference to be signed.
+            var reference = new Reference();
+            reference.Uri = "";
+            reference.DigestMethod = config.DigestMethod;
+
+            // Add an enveloped transformation to the reference.
+            var env = new XmlDsigEnvelopedSignatureTransform();
+            reference.AddTransform(env);
+
+            // Add the reference to the SignedXml object.
+            signedXml.AddReference(reference);
+
+            // Create a new KeyInfo object.
+            var keyInfo = new KeyInfo();
+
+            // Load the certificate into a KeyInfoX509Data object and add it to the KeyInfo object.
+            keyInfo.AddClause(new KeyInfoX509Data(publicCert));
+
+            // Add the KeyInfo object to the SignedXml object.
+            signedXml.KeyInfo = keyInfo;
+
+            // Compute the signature.
+            signedXml.ComputeSignature();
+
+            // Get the XML representation of the signature and save 
+            // it to an XmlElement object.
+            XmlElement xmlDigitalSignature = signedXml.GetXml();
+
+            // Append the element to the XML document.
+            xmlDoc.DocumentElement.AppendChild(xmlDoc.ImportNode(xmlDigitalSignature, true));
+
+            var sb = new StringBuilder();
+
+            using (XmlWriter xmlwr = XmlWriter.Create(sb))
+            {
+                xmlDoc.WriteTo(xmlwr);
+            }
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        ///     Sign an XML blob and retrun the signed xml blob with signature.
+        /// </summary>
+        /// <param name="xmlString"></param>
+        /// <param name="privateCert"></param>
+        /// <param name="publicCert"></param>
+        /// <returns></returns>
+        public static string Sign(string xmlString, X509Certificate2 privateCert, X509Certificate publicCert)
+        {
+            // Load an XML file into the XmlDocument object.
+            var xmlDoc = new XmlDocument();
+            xmlDoc.PreserveWhitespace = true;
+            using (var reader = new StringReader(xmlString))
+            {
+                using (var xmlReader = new XmlTextReader(reader))
+                {
+                    xmlReader.DtdProcessing = DtdProcessing.Prohibit;
+                    xmlDoc.Load(xmlReader);
+                }
+            }
+
+            // Prepare the signing key
+            var Key = (RSACryptoServiceProvider) privateCert.PrivateKey;
+
+            // Create a SignedXml object.
+            var signedXml = new SignedXml(xmlDoc);
+
+            // Add the key to the SignedXml document.
+            signedXml.SigningKey = Key;
+
+            // Create a reference to be signed.
+            var reference = new Reference();
+            reference.Uri = "";
+
+            // Add an enveloped transformation to the reference.
+            var env = new XmlDsigEnvelopedSignatureTransform();
+            reference.AddTransform(env);
+
+            // Add the reference to the SignedXml object.
+            signedXml.AddReference(reference);
+
+            // Create a new KeyInfo object.
+            var keyInfo = new KeyInfo();
+
+            // Load the certificate into a KeyInfoX509Data object and add it to the KeyInfo object.
+            keyInfo.AddClause(new KeyInfoX509Data(publicCert));
+
+            // Add the KeyInfo object to the SignedXml object.
+            signedXml.KeyInfo = keyInfo;
+
+            // Compute the signature.
+            signedXml.ComputeSignature();
+
+            // Get the XML representation of the signature and save 
+            // it to an XmlElement object.
+            XmlElement xmlDigitalSignature = signedXml.GetXml();
+
+            // Append the element to the XML document.
+            xmlDoc.DocumentElement.AppendChild(xmlDoc.ImportNode(xmlDigitalSignature, true));
+
+            var sb = new StringBuilder();
+
+            using (XmlWriter xmlwr = XmlWriter.Create(sb))
+            {
+                xmlDoc.WriteTo(xmlwr);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ CodeSignForXml/SignatureDescriptor.cs
@@ -0,0 +1,227 @@
+﻿// -----------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// This code is provided AS IS without warranty of any kind. 
+// -----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace CodeSignForXml
+{
+    /// <summary>
+    ///     Base class for SHA2 family Signature Description
+    /// </summary>
+    public class Sha2SignatureDescription : SignatureDescription
+    {
+        /// <summary>
+        ///     Hash Algorithm
+        /// </summary>
+        private readonly string _algorithm;
+
+        /// <summary>
+        ///     Construct an SHA2ignatureDescription object. The default settings for this object
+        ///     are:
+        ///     <list type="bullet">
+        ///         <item>Key algorithm - <see cref="RSACryptoServiceProvider" /></item>
+        ///         <item>Formatter algorithm - <see cref="RSAPKCS1SignatureFormatter" /></item>
+        ///         <item>Deformatter algorithm - <see cref="RSAPKCS1SignatureDeformatter" /></item>
+        ///     </list>
+        /// </summary>
+        public Sha2SignatureDescription(string algorithm, string digestAlgorithm)
+        {
+            if (string.IsNullOrWhiteSpace(algorithm))
+            {
+                throw new ArgumentException("algorithm cannot be null or white space only");
+            }
+
+            _algorithm = algorithm;
+
+            KeyAlgorithm = typeof(RSACryptoServiceProvider).FullName;
+            DigestAlgorithm = digestAlgorithm;
+            FormatterAlgorithm = typeof(RSAPKCS1SignatureFormatter).FullName;
+            DeformatterAlgorithm = typeof(RSAPKCS1SignatureDeformatter).FullName;
+        }
+
+        public override AsymmetricSignatureDeformatter CreateDeformatter(AsymmetricAlgorithm key)
+        {
+            if (key == null)
+                throw new ArgumentNullException("key");
+
+            var deformatter = new RSAPKCS1SignatureDeformatter(key);
+            deformatter.SetHashAlgorithm(_algorithm);
+            return deformatter;
+        }
+
+        public override AsymmetricSignatureFormatter CreateFormatter(AsymmetricAlgorithm key)
+        {
+            if (key == null)
+                throw new ArgumentNullException("key");
+
+            var formatter = new RSAPKCS1SignatureFormatter(key);
+            formatter.SetHashAlgorithm(_algorithm);
+            return formatter;
+        }
+    }
+
+    [SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", MessageId = "RSAPKCS",
+        Justification = "This casing is to match the existing RSAPKCS1SHA1SignatureDescription type")]
+    [SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", MessageId = "SHA",
+        Justification = "This casing is to match the use of SHA throughout the framework")]
+    public sealed class RSAPKCS1SHA256SignatureDescription : Sha2SignatureDescription
+    {
+        public RSAPKCS1SHA256SignatureDescription()
+            : base("SHA256", typeof(SHA256Managed).FullName)
+        {
+        }
+    }
+
+    [SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", MessageId = "RSAPKCS",
+        Justification = "This casing is to match the existing RSAPKCS1SHA1SignatureDescription type")]
+    [SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", MessageId = "SHA",
+        Justification = "This casing is to match the use of SHA throughout the framework")]
+    public sealed class RSAPKCS1SHA384SignatureDescription : Sha2SignatureDescription
+    {
+        public RSAPKCS1SHA384SignatureDescription()
+            : base("SHA384", typeof(SHA384Managed).FullName)
+        {
+        }
+    }
+
+    [SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", MessageId = "RSAPKCS",
+        Justification = "This casing is to match the existing RSAPKCS1SHA1SignatureDescription type")]
+    [SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", MessageId = "SHA",
+        Justification = "This casing is to match the use of SHA throughout the framework")]
+    public sealed class RSAPKCS1SHA512SignatureDescription : Sha2SignatureDescription
+    {
+        public RSAPKCS1SHA512SignatureDescription()
+            : base("SHA512", typeof(SHA512Managed).FullName)
+        {
+        }
+    }
+
+    /// <summary>
+    ///     configurations for XmlDSIG signing
+    /// </summary>
+    public class XmlDsigSigningConfig
+    {
+        public XmlDsigSigningConfig(string signatureMethod, string digestMethod, Type descriptionType)
+        {
+            if (String.IsNullOrEmpty(signatureMethod))
+            {
+                throw new ArgumentException("signatureMethod");
+            }
+
+            if (String.IsNullOrEmpty(digestMethod))
+            {
+                throw new ArgumentException("digestMethod");
+            }
+
+            if (descriptionType == null)
+            {
+                throw new ArgumentNullException("descriptionType");
+            }
+
+            if (!descriptionType.IsSubclassOf(typeof(SignatureDescription)))
+            {
+                throw new ArgumentException("descriptionType must be subclass of SignatureDescription");
+            }
+
+            SignatureDescription = descriptionType;
+            SignatureMethod = signatureMethod;
+            DigestMethod = digestMethod;
+        }
+
+        public string SignatureMethod { get; private set; }
+        public string DigestMethod { get; private set; }
+        public Type SignatureDescription { get; private set; }
+    }
+
+    /// <summary>
+    ///     XmlDisig utils class wrapped registration of SHA2 alogrithms
+    ///     For URI constants of Digest methods and signature method,
+    ///     please refer to http://msdn.microsoft.com/en-us/library/windows/desktop/dd979768(v=vs.85).aspx
+    /// </summary>
+    public static class XmlDsigUtils
+    {
+        public const string XmlDsigMoreRsaHA256Url = "http://www.w3.org/2001/04/xmldsig-more#rsa-sha256";
+        public const string XmlEncSHA256Url = "http://www.w3.org/2001/04/xmlenc#sha256";
+
+        public const string XmlDsigMoreRsaHA384Url = "http://www.w3.org/2001/04/xmldsig-more#rsa-sha384";
+        public const string XmlEncSHA384Url = "http://www.w3.org/2001/04/xmldsig-more#sha384";
+
+        public const string XmlDsigMoreRsaHA512Url = "http://www.w3.org/2001/04/xmldsig-more#rsa-sha512";
+        public const string XmlEncSHA512Url = "http://www.w3.org/2001/04/xmlenc#sha512";
+
+        private static readonly Dictionary<string, XmlDsigSigningConfig> XmlDsigconfigList =
+            new Dictionary<string, XmlDsigSigningConfig>(StringComparer.InvariantCultureIgnoreCase)
+            {
+                {
+                    Keys.SHA256,
+                    new XmlDsigSigningConfig(XmlDsigMoreRsaHA256Url, XmlEncSHA256Url,
+                        typeof (RSAPKCS1SHA256SignatureDescription))
+                },
+                {
+                    Keys.SHA384,
+                    new XmlDsigSigningConfig(XmlDsigMoreRsaHA384Url, XmlEncSHA384Url,
+                        typeof (RSAPKCS1SHA384SignatureDescription))
+                },
+                {
+                    Keys.SHA512,
+                    new XmlDsigSigningConfig(XmlDsigMoreRsaHA512Url, XmlEncSHA512Url,
+                        typeof (RSAPKCS1SHA512SignatureDescription))
+                },
+            };
+
+        /// <summary>
+        ///     Check if the signature method is supported
+        /// </summary>
+        /// <param name="signatureMethod"></param>
+        /// <returns></returns>
+        public static bool IsSigningMethodSupported(string signatureMethod)
+        {
+            return XmlDsigconfigList.Values.Any(config => config.SignatureMethod == signatureMethod);
+        }
+
+        /// <summary>
+        ///     Get XmlDSig signinig configuration
+        /// </summary>
+        /// <param name="key">the key of configurations, it cannot be null</param>
+        /// <returns>instances of XmlDsigSigningConfig, null will be returned if configuration item not found</returns>
+        public static XmlDsigSigningConfig GetSigningConfig(string key)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException("key");
+            }
+
+            XmlDsigSigningConfig config;
+            if (!XmlDsigconfigList.TryGetValue(key, out config))
+            {
+                config = null;
+            }
+
+            return config;
+        }
+
+        /// <summary>
+        ///     Register crypto algorithms
+        /// </summary>
+        public static void RegisterCryptoAlgorithms()
+        {
+            foreach (XmlDsigSigningConfig config in XmlDsigconfigList.Values)
+            {
+                CryptoConfig.AddAlgorithm(config.SignatureDescription, config.SignatureMethod);
+            }
+        }
+
+        public static class Keys
+        {
+            public const string SHA256 = "SHA256";
+            public const string SHA384 = "SHA384";
+            public const string SHA512 = "SHA512";
+        }
+    }
+}
